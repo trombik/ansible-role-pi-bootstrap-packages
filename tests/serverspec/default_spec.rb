155f5f50f5c235680516f90b0eb0ecaafc0f931b
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-12-08 19:11:41 +0900

    add pi_bootstrap_packages_gems

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 require 'serverspec'
 
-packages = %w( git-core vim libi2c-dev ruby )
+packages = %w( git-core vim libi2c-dev ruby ruby-dev )
+gems = %w( pi_piper )
 
 packages.each do |package|
   describe package(package) do
     it { should be_installed }
   end 
+end
+
+gems.each do |gem|
+  describe package(gem) do
+    it { should be_installed.by('gem') }
+  end
 end
